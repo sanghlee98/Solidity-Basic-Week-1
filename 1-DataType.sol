2e862f44d07505807c1d77c0ef46c6fd9333a8d9
sanghlee98 <sanghlee98@gmail.com>
2026-04-07 16:24:50 +0900

    initial commit

---
+++ 1-DataType.sol
@@ -0,0 +1,29 @@
+/*
+    todo : DataType 컨트랙트 생성 후 솔리디티의 기본 데이터 타입의 상태변수 선언 -> .sol 파일 컴파일 -> 컨트랙트 배포 및 호출 진행
+         : 솔리디티 기본 데이터 타입은 길이가 정해져 있습니다 -> 범위를 벗어나는 값 지정 후 오류 확인
+*/
+
+contract DataType {
+    // 1. bool 타입 변수 선언 및 논리연산
+    
+
+    // bool public isAnd = ;
+    // bool public isOr = ;
+    // bool public isNegate = ;
+
+
+    // 2. bytes 타입 변수 선언
+    // bytes4 public myBytes = ;
+
+    
+    // 3. address 타입 변수 선언
+    // address public myAddress = ;
+
+
+    // 4. int 타입 변수 선언
+    // int8 public myInt8 = ;
+
+
+    // 5. uint 타입 변수 선언
+    // uint8 public myUint8 = ;
+}
---
+++ 5-Event.sol
@@ -0,0 +1,17 @@
+/*
+    todo 1 : EventContract 생성 후 string, uint256 타입의 값을 받는 이벤트 myEvent 생성
+    todo 2 : 위에서 생성한 이벤트를 발생시키는 함수 작성 후 호출
+
+    -- 추가정보 --
+    1. 이벤트 선언 시 string은 단순한 타입의 정의일 뿐이기 때문에 memory 테그를 붙여 저장위치 명시할 필요 X
+*/
+
+contract EventContract {
+    // todo 1
+    
+
+    // todo 2
+    function demoFirstEvent(string memory _name, uint256 _age) public {
+        // emit myEvent(_name, _age);
+    }
+}
